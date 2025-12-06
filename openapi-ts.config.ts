973935d2214bc09de371dd27ff28a4276c3d708f
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-06 05:34:12 -0300

    added cofnig file for hey-openapi

---
+++ openapi-ts.config.ts
@@ -0,0 +1,25 @@
+import { type UserConfig } from "@hey-api/openapi-ts";
+
+const config: UserConfig = {
+    input: "./temp/spec.json",
+    output: {
+        path: "./packages/platform-service-sdk/generated-sdk",
+        format: "prettier",
+    },
+    plugins: [
+        {
+            name: "@hey-api/typescript",
+        },
+        {
+            name: "@hey-api/sdk",
+            asClass: false,
+        },
+        {
+            name: "@hey-api/client-fetch",
+            runtimeConfigPath:
+                "./packages/platform-service-sdk/runtime-config.ts",
+        },
+    ],
+};
+
+export default config;
