472ac5878005db3240f6dbf1c40fb8ca09c82127
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-19 10:51:57 -0300

    added role validation to the middleware

--- apps/private-cloud-frontend/src/proxy.ts
+++ apps/private-cloud-frontend/src/proxy.ts
@@ -2,15 +2,17 @@ import {
     FeatureNames,
     isFeatureFlagEnabled,
 } from "@repo/shared-utils/feature-flags";
-import { CookiesList } from "@/utils/cookies";
+import { CookiesList, getCookie, JwtCookie, MeResponse } from "@/utils/cookies";
 import { ApiRoutes, WebRoutes } from "./utils/routes";
 import { NextResponse, type NextRequest } from "next/server";
+import { RoleTypes } from "@repo/type-definitions";
 
 const protectedRoutes = [
     WebRoutes.HOME,
     WebRoutes.SEARCH,
     WebRoutes.DIRECTORY,
     WebRoutes.EPISODE,
+    WebRoutes.PENDING_USER_ACTIVATION,
 ];
 
 function checkIsProtectedRoute(path: WebRoutes) {
@@ -24,8 +26,10 @@ export async function proxy(request: NextRequest) {
     if (!isProtectedRoute) return NextResponse.next();
 
     try {
-        const session = request.cookies.get(CookiesList.USER);
-        const token = request.cookies.get(CookiesList.JWT);
+        const session = (await getCookie(
+            CookiesList.USER
+        )) as MeResponse | null;
+        const token = (await getCookie(CookiesList.JWT)) as JwtCookie | null;
         const ff = isFeatureFlagEnabled(FeatureNames.ENABLE_USERS_LOGIN);
 
         if (!ff && (!session || !token)) {
@@ -36,6 +40,21 @@ export async function proxy(request: NextRequest) {
 
         if (!session || !token) {
             return NextResponse.redirect(new URL(WebRoutes.LOGIN, request.url));
+        }
+
+        const hasValidRole =
+            session.role &&
+            (session.role.type === RoleTypes.ADULT_ANIME_WATCHER ||
+                session.role.type === RoleTypes.ANIME_WATCHER ||
+                session.role.type === RoleTypes.ANIME_PAGE_ADMIN);
+
+        if (
+            !hasValidRole &&
+            currentPath !== WebRoutes.PENDING_USER_ACTIVATION
+        ) {
+            return NextResponse.redirect(
+                new URL(WebRoutes.PENDING_USER_ACTIVATION, request.url)
+            );
         }
 
         return NextResponse.next();
