443f621ad548fef56fe9c4600888abc7a8ac2f77
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-19 10:54:03 -0300

    created not-found page

---
+++ apps/private-cloud-frontend/src/app/not-found.tsx
@@ -0,0 +1,33 @@
+import BottomNav from "@/components/BottomNavbar";
+import { Container, Typography } from "@mui/joy";
+import { FC } from "react";
+
+const NotFound: FC = () => {
+    return (
+        <Container
+            sx={{
+                textAlign: "center",
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                height: "100vh",
+            }}
+        >
+            <Typography
+                level="h1"
+                component="h1"
+                fontSize="xl4"
+                fontWeight="bold"
+                mt={4}
+            >
+                404 - Página No Encontrada
+            </Typography>
+            <Typography level="body-md" mt={2}>
+                Lo sentimos, la página que estás buscando no existe.
+            </Typography>
+            <BottomNav mainDirectories={[]} onlyGoBack disableNavbar />
+        </Container>
+    );
+};
+export default NotFound;
