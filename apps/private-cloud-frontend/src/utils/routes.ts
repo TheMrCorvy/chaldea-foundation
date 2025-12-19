f49127fff549c50c59c836a088c28c8ddcb00b9c
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-19 11:18:10 -0300

    implemented first iteration for v2 stream

--- apps/private-cloud-frontend/src/utils/routes.ts
+++ apps/private-cloud-frontend/src/utils/routes.ts
@@ -1,18 +1,22 @@
 export enum WebRoutes {
     HOME = "/",
-    LOGIN = "/login/",
-    REGISTER = "/register/",
-    SEARCH = "/search/",
-    DIRECTORY = "/directory/",
-    EPISODE = "/episode/",
-    PENDING_USER_ACTIVATION = "/pending-activation/",
+    LOGIN = "/login",
+    REGISTER = "/register",
+    SEARCH = "/search",
+    DIRECTORY = "/directory",
+    EPISODE = "/episode",
+    PENDING_USER_ACTIVATION = "/pending-activation",
+    NOT_FOUND = "/404",
 }
 
 export enum ApiRoutes {
-    MOCK_SESSION = "/api/set-mock-session/",
-    STREAM_MEDIA_SECURE = "/api/stream-media-secure",
+    MOCK_SESSION = "/api/set-mock-session",
+    STREAM_EPISODE = "/api/stream-episode",
 }
 
 export enum NasApiRoutes {
-    STREAM_MEDIA = "/api/stream-media/",
+    STREAM_MEDIA = "/api/v1/serve-episode",
+    V2_STREAM_MEDIA = "/api/v2/stream",
+    VIDEO_ONLY = "/api/v2/stream/video-only",
+    AUDIO_ONLY = "/api/v2/stream/audio-only",
 }
