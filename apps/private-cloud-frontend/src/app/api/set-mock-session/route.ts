e7477f179383d6498b768383ff6b0663ee74c804
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-22 19:36:43 -0300

    created mock session internal endpoint

---
+++ apps/private-cloud-frontend/src/app/api/set-mock-session/route.ts
@@ -0,0 +1,31 @@
+import { mockMeResponse, mockUserToken } from "@/mocks/mockedResponses";
+import {
+    FeatureNames,
+    isFeatureFlagEnabled,
+} from "@repo/shared-utils/feature-flags";
+import { CookiesList, setCookie } from "@/utils/cookies";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const host = req.headers.get("host");
+    const protocol = req.headers.get("x-forwarded-proto") || "https";
+    const redirectUrl = `${protocol}://${host}/`;
+
+    const response = NextResponse.redirect(redirectUrl);
+
+    if (isFeatureFlagEnabled(FeatureNames.ENABLE_USERS_LOGIN)) {
+        return response;
+    }
+
+    await setCookie({
+        name: CookiesList.USER,
+        value: mockMeResponse,
+    });
+
+    await setCookie({
+        name: CookiesList.JWT,
+        value: mockUserToken,
+    });
+
+    return response;
+}
