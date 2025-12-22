f0bacc5972bd03f161d7bbe81a006a4f5c4a84c3
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-22 19:32:54 -0300

    created login page

---
+++ apps/private-cloud-frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { FC } from "react";
+
+import TicketForm from "@/components/TicketLoginComponent";
+import {
+    FeatureNames,
+    isFeatureFlagEnabled,
+} from "@repo/shared-utils/feature-flags";
+import { notFound, redirect } from "next/navigation";
+import { CookiesList, getCookie } from "@/utils/cookies";
+
+export const metadata: Metadata = {
+    title: "Login - Servicio de Streaming Privado",
+    description: "Ingresar a la app de streaming privada.",
+    openGraph: {
+        title: "Login - Servicio de Streaming Privado",
+        description: "Ingresar a la app de streaming privada.",
+        images: [
+            {
+                url: "/kiyohime.webp",
+                width: 1200,
+                height: 630,
+                alt: "Kiyohime from Fate/Grand Order",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Login - Servicio de Streaming Privado",
+        description: "Ingresar a la app de streaming privada.",
+        images: ["/kiyohime.webp"],
+    },
+};
+
+const Login: FC = async () => {
+    if (!isFeatureFlagEnabled(FeatureNames.ENABLE_USERS_LOGIN)) {
+        notFound();
+    }
+
+    const userCookie = await getCookie(CookiesList.USER);
+    const jwtCookie = await getCookie(CookiesList.JWT);
+
+    if (userCookie && jwtCookie) {
+        redirect("/");
+    }
+
+    return (
+        <TicketForm
+            isRegisterForm={false}
+            ticketCode="X - X - X - X - X"
+            ticketNumber="X-X-X-X-X"
+            userName="Invitación exclusiva"
+            createdAt={new Date()}
+        />
+    );
+};
+
+export default Login;
