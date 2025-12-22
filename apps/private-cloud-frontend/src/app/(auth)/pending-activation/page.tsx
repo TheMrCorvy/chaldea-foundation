2fea6ed1a7e698769a9f60f7fa97d8e86515c46c
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-22 19:33:54 -0300

    created pending activation page

---
+++ apps/private-cloud-frontend/src/app/(auth)/pending-activation/page.tsx
@@ -0,0 +1,34 @@
+import BottomNav from "@/components/BottomNavbar";
+import { Container, Typography } from "@mui/joy";
+import { FC } from "react";
+
+const PendingActivation: FC = () => {
+    return (
+        <Container
+            sx={{
+                textAlign: "center",
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                height: "100vh",
+            }}
+        >
+            <Typography
+                level="h1"
+                component="h1"
+                fontSize="xl4"
+                fontWeight="bold"
+                mt={4}
+            >
+                Tu usuario ha sido registrado con éxito!
+            </Typography>
+            <Typography level="body-md" mt={2}>
+                Contáctate con el administrador del sitio para que tu rol sea
+                asignado.
+            </Typography>
+            <BottomNav mainDirectories={[]} disableNavbar />
+        </Container>
+    );
+};
+export default PendingActivation;
