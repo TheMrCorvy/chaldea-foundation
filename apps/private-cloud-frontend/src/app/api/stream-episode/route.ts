6d66059326c1c4aa1c9b2c08dec614ac7065262d
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-24 06:41:20 -0300

    created internal endpoint to redirect stream

---
+++ apps/private-cloud-frontend/src/app/api/stream-episode/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createReadStream, statSync } from "fs";
+import { join } from "path";
+import {
+    FeatureNames,
+    isFeatureFlagEnabled,
+} from "@repo/shared-utils/feature-flags";
+
+export async function GET(request: NextRequest) {
+    if (isFeatureFlagEnabled(FeatureNames.CONSUME_NAS_FILES)) {
+        return new NextResponse("There was an error", { status: 500 });
+    }
+
+    const filePath = join(
+        process.cwd(),
+        "src/mocks/mock-episodes/" + process.env.MOCK_FILE_NAME
+    );
+
+    // try {
+    const fileMetadata = statSync(filePath);
+    const range = request.headers.get("range");
+
+    if (!range) {
+        const headers = {
+            "Content-Type": "video/mp4",
+            "Content-Length": fileMetadata.size.toString(),
+            "Accept-Ranges": "bytes",
+        };
+        const stream = createReadStream(filePath);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return new NextResponse(stream as any, { headers });
+    }
+
+    const [startStr, endStr] = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(startStr, 10);
+    const end = endStr ? parseInt(endStr, 10) : fileMetadata.size - 1;
+
+    if (start >= fileMetadata.size || end >= fileMetadata.size) {
+        return new NextResponse("Range Not Satisfiable", { status: 416 });
+    }
+
+    const chunkSize = end - start + 1;
+    const stream = createReadStream(filePath, { start, end });
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${fileMetadata.size}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunkSize.toString(),
+        "Content-Type": "video/mp4",
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return new NextResponse(stream as any, {
+        headers,
+        status: 206, // Partial content
+    });
+    // } catch (error) {
+    //     console.error("Error streaming video:", error);
+    //     return new NextResponse("Internal Server Error", { status: 500 });
+    // }
+}
