935b6107311f8fd59493905fb695efc6ec583cd5
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-28 15:41:43 -0300

    created theme registry

---
+++ apps/private-cloud-frontend/src/lib/ThemeRegistry.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import { theme } from "./theme";
+
+export default function ThemeRegistry({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <CssVarsProvider
+            theme={theme}
+            defaultMode="dark"
+            modeStorageKey="joy-mode"
+            disableTransitionOnChange
+            // Prevent system preference from overriding
+            colorSchemeStorageKey="joy-color-scheme"
+            // Force attribute on HTML element
+            attribute="data-joy-color-scheme"
+        >
+            <CssBaseline />
+            {children}
+        </CssVarsProvider>
+    );
+}
