b8dfc6f17407e0ce777293f6eb02cbe06c418477
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-29 12:32:07 -0300

    refactored cookies util

--- apps/private-cloud-frontend/src/utils/cookies.ts
+++ apps/private-cloud-frontend/src/utils/cookies.ts
@@ -1,5 +1,9 @@
 import { cookies } from "next/headers";
 import { type RoleTypes } from "@repo/type-definitions";
+import {
+    FeatureNames,
+    isFeatureFlagEnabled,
+} from "@repo/shared-utils/feature-flags";
 
 export interface MeResponse {
     createdAt: Date;
@@ -7,10 +11,12 @@ export interface MeResponse {
     email: string;
     username: string;
     id: number;
+    documentId: string;
     provider: string;
     blocked: boolean;
     confirmed: boolean;
     role: Role;
+    ok: boolean;
 }
 
 export interface Role {
@@ -33,12 +39,46 @@ export interface JwtCookie {
 
 export type CookieFound = JwtCookie | MeResponse | null;
 
-export const getCookie = async (
-    cookieName: CookiesList
-): Promise<CookieFound> => {
+export type GetCookie = (cookieName: CookiesList) => Promise<CookieFound>;
+
+export const getCookie: GetCookie = async (cookieName) => {
     const cookieStore = await cookies();
     const cookie = cookieStore.get(cookieName)?.value;
     if (!cookie) return null;
 
     return JSON.parse(cookie).cookieObject;
+};
+
+export interface SetCookieParams {
+    name: string;
+    value: string | MeResponse;
+}
+
+export type SetCookie = (params: SetCookieParams) => Promise<void>;
+
+export const setCookie: SetCookie = async (params) => {
+    const cookieStore = await cookies();
+
+    const value =
+        typeof params.value === "string" ? { jwt: params.value } : params.value;
+
+    cookieStore.set({
+        name: params.name,
+        value: JSON.stringify({ cookieObject: value }),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        path: "/",
+    });
+};
+
+export const deleteCookie = async (cookieName: CookiesList) => {
+    const cookieStore = await cookies();
+
+    if (
+        cookieStore.get(cookieName) &&
+        isFeatureFlagEnabled(FeatureNames.ENABLE_USERS_LOGIN)
+    ) {
+        cookieStore.delete(cookieName);
+    }
 };
