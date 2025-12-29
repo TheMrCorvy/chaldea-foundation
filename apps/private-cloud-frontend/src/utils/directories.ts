68243b44eec2698eac6422fc32ece5c9ae48881e
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-12-29 12:33:47 -0300

    created an util to sort the directories

---
+++ apps/private-cloud-frontend/src/utils/directories.ts
@@ -0,0 +1,56 @@
+import { Directory } from "@repo/type-definitions";
+
+export interface DirectoryLink {
+    label: string;
+    url: string;
+    parent_directory?: string | null;
+    adult?: boolean;
+}
+
+export interface GroupedDirectories {
+    [key: string]: DirectoryLink[];
+}
+
+const groupDirectoriesByFirstCharacter = (
+    directories: DirectoryLink[]
+): GroupedDirectories => {
+    const grouped: GroupedDirectories = {};
+
+    directories.forEach((directory) => {
+        const firstChar = directory.label.charAt(0).toUpperCase();
+        if (!grouped[firstChar]) {
+            grouped[firstChar] = [];
+        }
+        grouped[firstChar].push(directory);
+    });
+
+    return grouped;
+};
+
+const sortDirectoriesByLabel = (
+    directories: DirectoryLink[]
+): DirectoryLink[] => {
+    return [...directories].sort((a, b) =>
+        a.label.localeCompare(b.label, undefined, {
+            numeric: true,
+            caseFirst: "lower",
+        })
+    );
+};
+
+const organizedDirectories = (directories: Directory[]): GroupedDirectories => {
+    const links: DirectoryLink[] = directories.map((dir) => ({
+        label: dir.display_name,
+        url: dir.documentId,
+        parent_directory: dir.parent_directory?.documentId || null,
+        adult: dir.adult as boolean,
+    }));
+
+    const sortedDirectories = sortDirectoriesByLabel(links);
+    const groupedDirectories =
+        groupDirectoriesByFirstCharacter(sortedDirectories);
+
+    return groupedDirectories;
+};
+
+export default organizedDirectories;
