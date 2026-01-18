a02d02a818c5f42119712ac6e17964ffac7975ff
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-01-18 11:59:37 -0300

    added layer

--- packages/shared-utils/src/logData.ts
+++ packages/shared-utils/src/logData.ts
@@ -20,6 +20,7 @@ type LayersAvailable =
     | "external_http_responses"
     | "video_streaming"
     | "video_streaming_subtitles"
+    | "bug_report"
     | "*"; // This layer will always be logged, use it for heavy errors
 
 export interface LogDataParams {
