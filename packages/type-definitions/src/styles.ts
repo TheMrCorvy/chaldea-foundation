6a37c8fc057329eaacab355471b22f19a244a4d7
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-01-25 11:51:07 -0300

    added support for MUI styles

--- packages/type-definitions/src/styles.ts
+++ packages/type-definitions/src/styles.ts
@@ -1,4 +1,5 @@
 import { SxProps } from "@mui/joy/styles/types";
+import type { SxProps as MUISxProps, Theme } from "@mui/material/styles";
 
 export interface Styles {
     [key: string]: SxProps;
@@ -17,3 +18,9 @@ interface BooleanFlags {
 export type StylesServiceProps = BaseProps & BooleanFlags;
 
 export type StylesService = (params?: StylesServiceProps) => Styles;
+
+export interface MUIStyles {
+    [key: string]: MUISxProps<Theme>;
+}
+
+export type MUIStylesService = (theme?: StylesServiceProps) => MUIStyles;
