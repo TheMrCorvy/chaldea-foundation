27b30342fe397145a17e0409cd713e5d2ba338fe
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-01-30 19:50:06 -0300

    added a small animation for the modal component

--- apps/mages-profile/src/components/Modal/index.tsx
+++ apps/mages-profile/src/components/Modal/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Grid } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 import { FC, ReactNode } from "react";
 import GlitchBackgroundCard from "../GlitchBacgkroundCard";
 
@@ -17,10 +18,6 @@ interface RenderContentParams {
 type RenderContent = (params: RenderContentParams) => ReactNode;
 
 const Modal: FC<ModalProps> = ({ children, open, onExit, isMobile }) => {
-    if (!open) {
-        return null;
-    }
-
     const rows = Array.from({ length: 3 }, (_, i) => i);
     const columns = Array.from({ length: 3 }, (_, i) => i);
 
@@ -81,63 +78,87 @@ const Modal: FC<ModalProps> = ({ children, open, onExit, isMobile }) => {
         return null;
     };
 
-    return (
-        <Box
-            onClick={onExit}
-            sx={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                height: "100dvh",
-                width: "100dvw",
-                backgroundColor: "rgba(0,0,0,0.5)",
+    const variants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    };
 
-                zIndex: 9999,
-            }}
-        >
-            <Grid container spacing={0}>
-                {rows.map((row) => (
-                    <Grid
-                        size={12}
-                        key={`modal-row-${row}`}
-                        spacing={0}
+    return (
+        <AnimatePresence>
+            {open && (
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={variants}
+                    transition={{ duration: 0.3 }}
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        height: "100dvh",
+                        width: "100dvw",
+                        zIndex: 9999,
+                    }}
+                >
+                    <Box
+                        onClick={onExit}
                         sx={{
-                            display: "flex",
-                            flexDirection: "row",
-                            height: calcRowHeight(row),
-                            borderBottom:
-                                row === 1
-                                    ? "1px solid rgba(255, 255, 255, 0.10)"
-                                    : "",
-                            borderTop:
-                                row === 1
-                                    ? "1px solid rgba(255, 255, 255, 0.10)"
-                                    : "",
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            height: "100dvh",
+                            width: "100dvw",
+                            backgroundColor: "rgba(0,0,0,0.5)",
+                            zIndex: 9999,
                         }}
                     >
-                        {columns.map((col) => (
-                            <Grid
-                                key={`modal-col-${col}`}
-                                spacing={0}
-                                sx={{
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    justifyContent: "end",
-                                    alignItems: "end",
-                                    padding: 0,
-                                }}
-                                size={calcColSize(col)}
-                            >
-                                {renderContent({
-                                    colNumber: col,
-                                    rowNumber: row,
-                                })}
-                            </Grid>
-                        ))}
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
+                        <Grid container spacing={0}>
+                            {rows.map((row) => (
+                                <Grid
+                                    size={12}
+                                    key={`modal-row-${row}`}
+                                    spacing={0}
+                                    sx={{
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        height: calcRowHeight(row),
+                                        borderBottom:
+                                            row === 1
+                                                ? "1px solid rgba(255, 255, 255, 0.10)"
+                                                : "",
+                                        borderTop:
+                                            row === 1
+                                                ? "1px solid rgba(255, 255, 255, 0.10)"
+                                                : "",
+                                    }}
+                                >
+                                    {columns.map((col) => (
+                                        <Grid
+                                            key={`modal-col-${col}`}
+                                            spacing={0}
+                                            sx={{
+                                                display: "flex",
+                                                flexDirection: "row",
+                                                justifyContent: "end",
+                                                alignItems: "end",
+                                                padding: 0,
+                                            }}
+                                            size={calcColSize(col)}
+                                        >
+                                            {renderContent({
+                                                colNumber: col,
+                                                rowNumber: row,
+                                            })}
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 };
 
