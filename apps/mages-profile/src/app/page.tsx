c23fb2ebd93d4a3f433b61b465afff7c1d920fdd
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-01-30 12:58:03 -0300

    removed hologram text

--- apps/mages-profile/src/app/page.tsx
+++ apps/mages-profile/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import GlobeWrapper from "@/components/Globe/GlobeWrapper";
-import HologramGlitchText from "@/components/HologramGlitchText";
 import MagesData from "@/components/MagesData";
 import StarryContainer from "@/components/StarryContainer";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
@@ -9,33 +8,9 @@ import { useMediaQuery } from "@/hooks/useMediaQuery";
 export default function HomePage() {
     const isMobile = useMediaQuery().max.width("sm");
 
-    const textPosition = () => {
-        if (isMobile) {
-            return {
-                bottom: "20%",
-            };
-        }
-
-        return {
-            top: "7%",
-        };
-    };
-
     return (
         <StarryContainer>
             <>
-                <HologramGlitchText
-                    sx={{
-                        position: "absolute",
-                        ...textPosition(),
-                        left: "50%",
-                        transform: "translateX(-50%)",
-                        whiteSpace: "nowrap",
-                    }}
-                    variant={isMobile ? "subtitle1" : "h5"}
-                >
-                    Exploratio anima in cosmi somniorum
-                </HologramGlitchText>
                 <GlobeWrapper isMobile={isMobile} />
                 <MagesData isMobile={isMobile} />
             </>
