5a1a3e33a6f54bb83dbf68f96fb9cc5b51a756b1
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-02-08 21:14:12 -0300

    installed speed insights for vercel

--- apps/mages-profile/src/app/layout.tsx
+++ apps/mages-profile/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeRegistry } from "@/components/ThemeRegistry";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <Analytics />
+            <SpeedInsights />
             <ThemeRegistry>
                 <body className={inter.className}>{children}</body>
             </ThemeRegistry>
