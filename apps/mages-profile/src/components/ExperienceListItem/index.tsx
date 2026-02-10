01f074e23f6adf48aeafe55b819d4588b4d0f1de
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-02-10 09:31:42 -0300

    created PoC for ExperienceListItem component

---
+++ apps/mages-profile/src/components/ExperienceListItem/index.tsx
@@ -0,0 +1,148 @@
+import { Box, Typography } from "@mui/material";
+import { FC } from "react";
+
+export interface ExperienceItem {
+    role: string;
+    orientation?: string; // e.g. "Frontend Oriented"
+    company: string;
+    client?: string;
+    startDate: string; // "Dec 2025"
+    endDate: string; // "Present" | "Oct 2023"
+    description: string;
+}
+
+export interface ExperienceListItemProps {
+    experience: ExperienceItem;
+    isMobile?: boolean;
+}
+
+const ExperienceListItem: FC<ExperienceListItemProps> = ({
+    experience,
+    isMobile,
+}) => {
+    const {
+        role,
+        orientation,
+        company,
+        client,
+        startDate,
+        endDate,
+        description,
+    } = experience;
+
+    return (
+        <Box
+            component="section"
+            sx={{
+                display: "flex",
+                flexDirection: "row",
+            }}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    height: "100%",
+                    justifyContent: "start",
+                    alignContent: "start",
+                    alignItems: "start",
+                    verticalAlign: "start",
+                    px: 1,
+                    pt: 0.4,
+                }}
+            >
+                <Typography
+                    variant="subtitle1"
+                    sx={{
+                        fontWeight: 600,
+                        color: "common.white",
+                        // lineHeight: 1.3,
+                    }}
+                >
+                    •
+                </Typography>
+            </Box>
+            {/* Title line */}
+            <Box component="span">
+                <Box
+                    component="span"
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 1,
+                        verticalAlign: "center",
+                    }}
+                >
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontWeight: 600,
+                            color: "common.white",
+                            lineHeight: 1.3,
+                        }}
+                    >
+                        {role}
+                    </Typography>
+                    <Typography
+                        component="span"
+                        sx={{
+                            color: "grey.300",
+                            display: "block",
+                            lineHeight: 1.3,
+                            fontSize: "0.8rem",
+                        }}
+                    >
+                        {orientation && ` (${orientation})`}
+                    </Typography>
+                </Box>
+                <Box
+                    component="span"
+                    sx={{
+                        display: "flex",
+                        width: "100%",
+                        justifyContent: "space-between",
+                        flexDirection: "row",
+                        pt: 0.5,
+                        mb: 1,
+                    }}
+                >
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontWeight: 500,
+                            color: "common.white",
+                            lineHeight: 1.3,
+                        }}
+                    >
+                        {company} {client && ` / ${client}`}
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontWeight: 400,
+                            color: "common.white",
+                            lineHeight: 1.3,
+                        }}
+                    >
+                        {startDate} → {endDate}
+                    </Typography>
+                </Box>
+
+                {/* Description */}
+                <Typography
+                    variant={isMobile ? "caption" : "body2"}
+                    sx={{
+                        color: isMobile ? "grey.100" : "grey.400",
+                        display: "block",
+                        mt: 0.5,
+                        lineHeight: 1.4,
+                    }}
+                >
+                    {description}
+                </Typography>
+            </Box>
+        </Box>
+    );
+};
+
+export default ExperienceListItem;
