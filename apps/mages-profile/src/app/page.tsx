fa87ce19491465b98d6f417bab1d09d76c5ba9e1
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-02-11 07:06:37 -0300

    added http request to main page

--- apps/mages-profile/src/app/page.tsx
+++ apps/mages-profile/src/app/page.tsx
@@ -1,19 +1,63 @@
-"use client";
+import MainPage from "@/components/ClientWrapper";
+import { SoundProvider } from "@/contexts/SoundContext";
+import PlatformService from "@repo/platform-service-sdk";
+import { logData } from "@repo/shared-utils/log-data";
+import { redirect } from "next/navigation";
 
-import GlobeWrapper from "@/components/Globe/GlobeWrapper";
-import MagesData from "@/components/MagesData";
-import StarryContainer from "@/components/StarryContainer";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
+export default async function HomePage() {
+    const token = process.env.PLATFORM_SERVICE_KEY || "";
 
-export default function HomePage() {
-    const isMobile = useMediaQuery().max.width("sm");
+    if (!token) {
+        logData({
+            title: "Critical env variable not found",
+            layer: "*",
+            timeStamp: true,
+            addSeparatorAfter: true,
+            addSpaceAfter: true,
+            data: {
+                token,
+            },
+            type: "error",
+        });
+
+        return redirect("/404");
+    }
+
+    const platformService = new PlatformService();
+    platformService.setJWT(token);
+
+    const { data } = await platformService.call(
+        "aDynamicPageGetADynamicPages",
+        {
+            query: {
+                filters: {
+                    slug: {
+                        $eq: "cv",
+                    },
+                },
+                populate: "*",
+            },
+        }
+    );
+
+    if (!data || !data.data) {
+        logData({
+            title: "The dynamic page requested doesn't exists",
+            layer: "*",
+            addSeparatorAfter: true,
+            timeStamp: true,
+            addSpaceAfter: true,
+            data,
+            type: "error",
+        });
+        return redirect("/404");
+    }
+
+    console.log(data);
 
     return (
-        <StarryContainer>
-            <>
-                <GlobeWrapper isMobile={isMobile} />
-                <MagesData isMobile={isMobile} />
-            </>
-        </StarryContainer>
+        <SoundProvider>
+            <MainPage />
+        </SoundProvider>
     );
 }
