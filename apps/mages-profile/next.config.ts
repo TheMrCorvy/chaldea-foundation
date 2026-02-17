3c3101e955923c68db407bdba99445de9543c77f
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-02-17 05:45:47 -0300

    added image source url

--- apps/mages-profile/next.config.ts
+++ apps/mages-profile/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "admin.chaldea.foundation",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "1337",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
