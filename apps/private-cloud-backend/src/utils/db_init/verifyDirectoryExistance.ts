4733471ab6451c719af63300928b1a1bdba3d457
Gonzalo Corvalán <salvatorehakase@Mac-mini-de-Gonzalo.local>
2026-03-21 09:40:05 -0300

    refactored episode existance verification

---
+++ apps/private-cloud-backend/src/utils/db_init/verifyDirectoryExistance.ts
@@ -0,0 +1,7 @@
+import { VerifyDirectoryExistance } from '../../types/dbInit';
+
+const verifyDirectoryExistance: VerifyDirectoryExistance = async () => {
+    return {};
+};
+
+export default verifyDirectoryExistance;
