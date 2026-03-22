2f4aa1d1a3b158f6c6d85463f4e41c535e17136f
Gonzalo Corvalán <salvatorehakase@Mac-mini-de-Gonzalo.local>
2026-03-22 15:57:34 -0300

    refactored db initialization script

--- apps/private-cloud-backend/src/utils/db_init/verifyDirectoryExistance.ts
+++ apps/private-cloud-backend/src/utils/db_init/verifyDirectoryExistance.ts
@@ -1,7 +1,63 @@
+import PlatformService from '@repo/platform-service-sdk';
 import { VerifyDirectoryExistance } from '../../types/dbInit';
+import { logData } from '@repo/shared-utils/log-data';
 
-const verifyDirectoryExistance: VerifyDirectoryExistance = async () => {
-    return {};
+const verifyDirectoryExistance: VerifyDirectoryExistance = async ({
+    directory,
+    failedDirectories,
+    skippedDirectories,
+}) => {
+    const platformService = new PlatformService();
+
+    const directoryAlreadyExists = await platformService.call('bDirectoryGetBDirectories', {
+        query: {
+            filters: {
+                path: {
+                    $eq: directory.directory_path,
+                },
+            },
+        },
+    });
+
+    if (directoryAlreadyExists.error || !directoryAlreadyExists.data.data) {
+        failedDirectories.push({ ...directory, error: directoryAlreadyExists });
+        logData({
+            layer: '*',
+            title: `Error checking if directory exists in strapi`,
+            data: directoryAlreadyExists.error,
+            addSpaceAfter: true,
+        });
+
+        return {
+            error: {
+                strapiError: directoryAlreadyExists.error,
+                strapiResponse: directoryAlreadyExists.data,
+            },
+            failed: true,
+            skipped: false,
+        };
+    }
+
+    if (directoryAlreadyExists.data.data.length > 0) {
+        skippedDirectories.push(directory);
+
+        logData({
+            layer: '*',
+            title: 'Directory already exists in strapi, skipping...',
+            addSpaceAfter: true,
+        });
+
+        return {
+            skipped: true,
+            failed: false,
+            directory: directoryAlreadyExists.data.data,
+        };
+    }
+    return {
+        skipped: false,
+        failed: false,
+        directory: directoryAlreadyExists.data.data,
+    };
 };
 
 export default verifyDirectoryExistance;
