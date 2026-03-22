3cf582d47700da07f2295e237adf0d711cccc348
Gonzalo Corvalán <salvatorehakase@Mac-mini-de-Gonzalo.local>
2026-03-22 15:57:26 -0300

    refactored db initialization script

---
+++ apps/private-cloud-backend/src/utils/db_init/envVerification.ts
@@ -0,0 +1,36 @@
+import { logData } from '@repo/shared-utils/log-data';
+import { VerifyEnvResult } from '../../types/dbInit';
+
+const envVerification = (): VerifyEnvResult => {
+    const secureBasePath = process.env.SECURE_BASE_PATH || '';
+    const initiumIter: string[] = process.env.INITIAL_PATH ? JSON.parse(process.env.INITIAL_PATH) : [];
+    const excludedParents: string[] = process.env.EXCLUDED_PARENTS ? JSON.parse(process.env.EXCLUDED_PARENTS) : [];
+    const strapiApiKey = process.env.STRAPI_API_KEY;
+
+    if (!initiumIter || initiumIter.length < 1 || !strapiApiKey || !excludedParents || !secureBasePath) {
+        logData({
+            title: 'Some env variables are not set',
+            data: {
+                initiumIter,
+                strapiApiKey,
+                excludedParents,
+                secureBasePath,
+            },
+            type: 'error',
+            layer: '*',
+            addSpaceAfter: true,
+            addSpaceBefore: true,
+            addSeparatorAfter: true,
+        });
+        throw new Error('Environment variables are not set.');
+    }
+
+    return {
+        secureBasePath,
+        initiumIter,
+        excludedParents,
+        strapiApiKey,
+    };
+};
+
+export default envVerification;
