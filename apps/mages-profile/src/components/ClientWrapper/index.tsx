fbaa843373eabac3ef69c5cf4f3826efb740e32c
Gonzalo Salvador corvalán <gonzalosalvadorcorvalan@gmail.com>
2026-03-25 07:16:50 -0300

    small changes in the logic

--- apps/mages-profile/src/components/ClientWrapper/index.tsx
+++ apps/mages-profile/src/components/ClientWrapper/index.tsx
@@ -7,7 +7,6 @@ import useClickAnimationAndSounds from "@/hooks/useClickAnimationAndSounds";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import {
     LayoutLandingHero,
-    LayoutWorkExperienceSection,
     StrapiSection,
 } from "@repo/type-definitions/dynamic-page";
 import { FC } from "react";
@@ -26,16 +25,13 @@ const MainPage: FC<MainPageProps> = ({ sections, imagesBaseUrl }) => {
     const magesData: MagesDataProps = {
         name: magesDataSection.title as string,
         position: magesDataSection.highlighted_subtitle,
-        profile_image: `${imagesBaseUrl}${magesDataSection.profile_image.url}`,
-        commands: `${imagesBaseUrl}${magesDataSection.commands.url}`,
+        profile_image: imagesBaseUrl + magesDataSection.profile_image.url,
+        commands: imagesBaseUrl + magesDataSection.commands.url,
     };
 
     return (
         <StarryContainer>
-            <GlobeWrapper
-                isMobile={isMobile}
-                experienceSection={sections[1] as LayoutWorkExperienceSection}
-            />
+            <GlobeWrapper isMobile={isMobile} sections={sections} />
             <MagesData {...magesData} isMobile={isMobile} />
         </StarryContainer>
     );
