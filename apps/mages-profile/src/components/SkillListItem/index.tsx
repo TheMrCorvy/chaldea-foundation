1322a1a9f1d66a501babb59009678b36e79a1343
Gonzalo Salvador corvalán <gonzalosalvadorcorvalan@gmail.com>
2026-03-29 12:18:34 -0300

    created SkillListItem component

---
+++ apps/mages-profile/src/components/SkillListItem/index.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import IconComponent, { IconName } from "../IconComponent";
+import { Tooltip, Typography } from "@mui/material";
+
+export interface SkillListItemProps {
+    icon: IconName;
+    title: string;
+    popover?: string;
+    size?: "inherit" | "small" | "medium" | "large";
+}
+
+const SkillListItem: FC<SkillListItemProps> = ({
+    icon,
+    title,
+    popover,
+    size = "medium",
+}) => {
+    return (
+        <span
+            style={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                flexDirection: "column",
+                textAlign: "center",
+            }}
+        >
+            <IconComponent size={size} icon={icon} />
+            <Typography variant="caption" fontSize={10} align="center">
+                {title}
+            </Typography>
+            {popover && (
+                <Tooltip title={popover} placement="top">
+                    <IconComponent icon="Info" size="small" />
+                </Tooltip>
+            )}
+        </span>
+    );
+};
+
+export default SkillListItem;
