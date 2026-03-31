12185345be8a0389e48c8ba7a022b13772ac6729
Gonzalo Salvador corvalán <gonzalosalvadorcorvalan@gmail.com>
2026-03-31 06:37:33 -0300

    now projects section is scrollable

--- apps/mages-profile/src/components/MainPageModalSections/ProjectsSection.tsx
+++ apps/mages-profile/src/components/MainPageModalSections/ProjectsSection.tsx
@@ -18,6 +18,7 @@ const ProjectsSection: FC<ProjectsSectionProps> = ({
             id={"main-page-projects-section-" + component_id}
             sx={{
                 height: "100%",
+                width: "100%",
                 position: "relative",
                 display: "flex",
                 flexDirection: "column",
@@ -27,6 +28,14 @@ const ProjectsSection: FC<ProjectsSectionProps> = ({
                 pl: "11px",
                 pr: isMobile ? 0 : "11px",
                 pt: isMobile ? 0 : "11px",
+                pb: "11px",
+                overflowY: "hidden",
+                overflowX: "hidden",
+                "&::-webkit-scrollbar": { width: "1%" },
+                "&::-webkit-scrollbar-thumb": {
+                    backgroundColor: "rgba(127, 214, 255, 0.45)",
+                    borderRadius: "999px",
+                },
             }}
         >
             {projects.map((project, i) => (
