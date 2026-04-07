e69b9afde6cdfff4bdad81151cf7dae31089e273
Gonzalo Salvador corvalán <gonzalosalvadorcorvalan@gmail.com>
2026-04-07 18:28:19 -0300

    extracted UI effects into its own component to re-use it later

--- apps/mages-profile/src/components/ClientSideProjectsPage/index.tsx
+++ apps/mages-profile/src/components/ClientSideProjectsPage/index.tsx
@@ -1,121 +1,26 @@
-"use client";
-
 import { SectionsProjectsSection } from "@repo/type-definitions/dynamic-page";
-import GlitchButton from "../GlitchButton";
 import MagicBento from "../MagicBento";
 import StarryContainer from "../StarryContainer";
 import { FC } from "react";
-import { useRouter } from "next/navigation";
-import { motion } from "framer-motion";
-import { CSSProperties } from "@mui/material";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
-import ToggleSound from "../ToggleSound";
-import useClickAnimationAndSounds from "@/hooks/useClickAnimationAndSounds";
+import ClientSideUiEffects from "../DynamicZone/ClientSideUI/ClientSideUiEffects";
 
 export interface ClientSideProjectsPageProps {
     projectsSection: SectionsProjectsSection;
     appSection?: string;
 }
 
-const containerVariants = {
-    hidden: {},
-    visible: {
-        transition: {
-            staggerChildren: 0.5,
-        },
-    },
-    exit: {
-        transition: {
-            staggerChildren: 0.2,
-            staggerDirection: -1,
-        },
-    },
-};
-
-const itemVariants = {
-    hidden: { y: -20, opacity: 0 },
-    visible: {
-        y: 0,
-        opacity: 1,
-        transition: {
-            duration: 0.5,
-        },
-    },
-    exit: {
-        y: -20,
-        opacity: 0,
-        transition: {
-            duration: 0.5,
-        },
-    },
-};
-
 const ClientSideProjectsPage: FC<ClientSideProjectsPageProps> = ({
     projectsSection,
     appSection,
 }) => {
-    const router = useRouter();
     const isMobile = useMediaQuery().max.width("sm");
-    const asideStyle: CSSProperties = {
-        position: "absolute",
-        backgroundColor: "transparent",
-        zIndex: 2,
-        display: "flex",
-        flexDirection: "column",
-        pointerEvents: "auto",
-    };
-
-    useClickAnimationAndSounds();
-
-    const handleClick = () => {
-        let route = "/";
-
-        if (appSection) {
-            route += appSection;
-        }
-
-        return router.push(route);
-    };
 
     return (
         <StarryContainer>
-            <motion.aside
-                style={{
-                    ...asideStyle,
-                    top: isMobile ? 10 : 16,
-                    left: isMobile ? 10 : 16,
-                    padding: isMobile ? 0 : "1rem",
-                    gap: 10,
-                }}
-                variants={containerVariants}
-                initial="hidden"
-                animate={"visible"}
-            >
-                <motion.div variants={itemVariants}>
-                    <GlitchButton
-                        label="Back"
-                        dataSound="page_change"
-                        cornerVariant="right"
-                        onClick={handleClick}
-                    />
-                </motion.div>
-            </motion.aside>
-            <motion.aside
-                style={{
-                    ...asideStyle,
-                    top: isMobile ? 10 : 16,
-                    right: isMobile ? 10 : 16,
-                    padding: isMobile ? 0 : "1rem",
-                    gap: 10,
-                }}
-                variants={containerVariants}
-                initial="hidden"
-                animate={"visible"}
-            >
-                <motion.div variants={itemVariants}>
-                    <ToggleSound useLeftSideBtn />
-                </motion.div>
-            </motion.aside>
+            <ClientSideUiEffects
+                routerPush={appSection ? "/" + appSection : "/"}
+            />
             <section
                 style={{
                     marginTop: isMobile ? "4rem" : 0,
---
+++ apps/mages-profile/src/components/DynamicZone/ClientSideUI/ClientSideUiEffects.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import GlitchButton from "../../GlitchButton";
+import { FC } from "react";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import { CSSProperties } from "@mui/material";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import ToggleSound from "../../ToggleSound";
+import useClickAnimationAndSounds from "@/hooks/useClickAnimationAndSounds";
+
+export interface ClientSideUiEffectsProps {
+    routerPush: string;
+}
+
+const containerVariants = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.5,
+        },
+    },
+    exit: {
+        transition: {
+            staggerChildren: 0.2,
+            staggerDirection: -1,
+        },
+    },
+};
+
+const itemVariants = {
+    hidden: { y: -20, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+        },
+    },
+    exit: {
+        y: -20,
+        opacity: 0,
+        transition: {
+            duration: 0.5,
+        },
+    },
+};
+
+const ClientSideUiEffects: FC<ClientSideUiEffectsProps> = ({ routerPush }) => {
+    const router = useRouter();
+    const isMobile = useMediaQuery().max.width("sm");
+    const asideStyle: CSSProperties = {
+        position: "absolute",
+        backgroundColor: "transparent",
+        zIndex: 2,
+        display: "flex",
+        flexDirection: "column",
+        pointerEvents: "auto",
+    };
+
+    useClickAnimationAndSounds();
+
+    const handleClick = () => {
+        router.push(routerPush);
+    };
+
+    return (
+        <>
+            <motion.aside
+                style={{
+                    ...asideStyle,
+                    top: isMobile ? 10 : 16,
+                    left: isMobile ? 10 : 16,
+                    padding: isMobile ? 0 : "1rem",
+                    gap: 10,
+                }}
+                variants={containerVariants}
+                initial="hidden"
+                animate={"visible"}
+            >
+                <motion.div variants={itemVariants}>
+                    <GlitchButton
+                        label="Back"
+                        dataSound="page_change"
+                        cornerVariant="right"
+                        onClick={handleClick}
+                    />
+                </motion.div>
+            </motion.aside>
+            <motion.aside
+                style={{
+                    ...asideStyle,
+                    top: isMobile ? 10 : 16,
+                    right: isMobile ? 10 : 16,
+                    padding: isMobile ? 0 : "1rem",
+                    gap: 10,
+                }}
+                variants={containerVariants}
+                initial="hidden"
+                animate={"visible"}
+            >
+                <motion.div variants={itemVariants}>
+                    <ToggleSound useLeftSideBtn />
+                </motion.div>
+            </motion.aside>
+        </>
+    );
+};
+
+export default ClientSideUiEffects;
