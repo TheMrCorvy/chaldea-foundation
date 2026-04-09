aaa7f6a45e2368e794c4de70ecc55d010b521f99
Gonzalo Salvador corvalán <gonzalosalvadorcorvalan@gmail.com>
2026-04-09 08:29:30 -0300

    removed unnecessary component

--- apps/mages-profile/src/components/DynamicZone/DynamicContactSection/index.tsx
+++
@@ -1,8 +0,0 @@
-import { SectionsContactSection } from "@repo/type-definitions/dynamic-page";
-import { FC } from "react";
-
-const DynamicContactSection: FC<SectionsContactSection> = () => {
-    return null;
-};
-
-export default DynamicContactSection;
