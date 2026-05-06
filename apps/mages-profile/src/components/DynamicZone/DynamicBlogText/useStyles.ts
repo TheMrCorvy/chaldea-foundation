b5b578124a01f5ed40223573e66a9d7f3dcfe9f7
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-05-06 07:13:45 -0300

    refactored main layout to be able to decide if using client-side or ssr

--- apps/mages-profile/src/components/DynamicZone/DynamicBlogText/useStyles.ts
+++
@@ -1,9 +0,0 @@
-import { MUIStylesService } from "@repo/type-definitions/styles";
-
-const useStyles: MUIStylesService = () => {
-    return {
-        root: { marginBottom: "2rem", width: "100%", maxWidth: "1300px" },
-    };
-};
-
-export default useStyles;
