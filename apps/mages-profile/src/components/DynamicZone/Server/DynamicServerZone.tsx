feac4a0b86da2abf31081c31dc765b91ede8a41a
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-05-07 10:11:53 -0300

    refactored main layout to be able to decide if using client-side or ssr

---
+++ apps/mages-profile/src/components/DynamicZone/Server/DynamicServerZone.tsx
@@ -0,0 +1,41 @@
+"use server";
+
+import { Box } from "@mui/material";
+import { StrapiSection } from "@repo/type-definitions/dynamic-page";
+import { FC } from "react";
+import DynamicZoneComponent from ".";
+
+export interface DynamycServerZone {
+    sections: StrapiSection[];
+    imageBaseUrl: string;
+}
+
+const DynamycServerZone: FC<DynamycServerZone> = ({
+    sections,
+    imageBaseUrl,
+}) => {
+    return (
+        <Box
+            component="article"
+            sx={{
+                px: { xs: 2, md: 6, lg: 7, xl: 8 },
+                py: { xs: 6, lg: 12 },
+                display: "flex",
+                flexDirection: "column",
+                gap: { xs: 6, md: 8, lg: 16, xl: 22 },
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            {sections.map((section) => (
+                <DynamicZoneComponent
+                    section={section}
+                    key={section.component_id}
+                    imageBaseUrl={imageBaseUrl}
+                />
+            ))}
+        </Box>
+    );
+};
+
+export default DynamycServerZone;
