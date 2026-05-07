c95615823f0b57cb77cd51333a0b2ada3f0e8867
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-05-07 10:11:57 -0300

    refactored main layout to be able to decide if using client-side or ssr

---
+++ apps/mages-profile/src/components/DynamicZone/Server/index.tsx
@@ -0,0 +1,114 @@
+"use server";
+
+import {
+    BlogHero,
+    BlogImageComponent,
+    BlogText,
+    LayoutDescriptionWithChipsList,
+    LayoutForm,
+    LayoutIcon,
+    LayoutLandingHero,
+    LayoutLink,
+    LayoutLogoLoop,
+    LayoutWorkExperienceSection,
+    SectionsProjectsSection,
+    StrapiPDFComponent,
+    StrapiSection,
+} from "@repo/type-definitions/dynamic-page";
+import { FC } from "react";
+import DynamicProjectsSection from "./DynamicProjectsSection";
+import DynamicLandingHeroSection from "./DynamicLandingHeroSection";
+import DynamicWorkExperienceSection from "./DynamicWorkExperienceSection";
+import DynamicBlogText from "./DynamicBlogText";
+import DynamicDescriptionWithChipsList from "./DynamicDescriptionWithChipsList";
+import DynamicBlogHero from "./DynamicBlogHero";
+import DynamicPdfFile from "./DynamicPdfFile";
+import DynamicLink from "./DynamicLink";
+import IconComponent from "../../IconComponent";
+import DynamicForm from "./DynamicForm";
+import DynamicBlogImage from "./DynamicBlogImage";
+import DynamicLogoLoop from "./DynamicLogoLoop";
+
+export interface DynamicZoneComponentProps {
+    section: StrapiSection;
+    imageBaseUrl: string;
+}
+
+const DynamicZoneComponentForServer: FC<DynamicZoneComponentProps> = ({
+    section,
+    imageBaseUrl,
+}) => {
+    switch (section.__component) {
+        case "sections.projects-section":
+            return (
+                <DynamicProjectsSection
+                    {...(section as SectionsProjectsSection)}
+                    imageBaseUrl={imageBaseUrl}
+                />
+            );
+
+        case "sections.landing-hero-section":
+            return (
+                <DynamicLandingHeroSection
+                    imageBaseUrl={imageBaseUrl}
+                    {...(section as LayoutLandingHero)}
+                />
+            );
+
+        case "sections.work-experience-section":
+            return (
+                <DynamicWorkExperienceSection
+                    {...(section as LayoutWorkExperienceSection)}
+                />
+            );
+
+        case "blog.blog-text":
+            return <DynamicBlogText {...(section as BlogText)} />;
+
+        case "sections.contact-section":
+            return null; // This section is going to be replaced with a normal form
+
+        case "layout.description-with-chips-list":
+            return (
+                <DynamicDescriptionWithChipsList
+                    {...(section as LayoutDescriptionWithChipsList)}
+                />
+            );
+
+        case "blog.blog-hero":
+            return <DynamicBlogHero {...(section as BlogHero)} />;
+
+        case "blog.pdf-file":
+            return (
+                <DynamicPdfFile
+                    {...(section as StrapiPDFComponent)}
+                    filesBaseUrl={imageBaseUrl}
+                />
+            );
+
+        case "layout.link":
+            return <DynamicLink {...(section as LayoutLink)} />;
+
+        case "layout.icon":
+            return (
+                <IconComponent
+                    {...(section as LayoutIcon)}
+                    id={section.component_id}
+                />
+            );
+
+        case "layout.form":
+            return <DynamicForm {...(section as LayoutForm)} />;
+
+        case "blog.blog-image":
+            return <DynamicBlogImage {...(section as BlogImageComponent)} />;
+
+        case "layout.logo-loop":
+            return <DynamicLogoLoop {...(section as LayoutLogoLoop)} />;
+
+        default:
+            return null;
+    }
+};
+
+export default DynamicZoneComponentForServer;
