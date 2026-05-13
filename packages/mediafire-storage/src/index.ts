860a03b6ef7e12f9ca23c5e8a78a25a2912240c4
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-05-13 06:58:56 -0300

    removed mediafire integration since they don't give api keys anymore

--- packages/mediafire-storage/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./client.js";
-
-export * from "./types.js";
-
-export * from "./upload.js";
-export * from "./download.js";
-export * from "./delete.js";
-
-export * from "./auth.js";
