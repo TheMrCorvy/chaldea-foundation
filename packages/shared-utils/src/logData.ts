9f123c00ced4db0f640501f945e29f4078ef9c24
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-05-29 14:00:45 -0300

    added a new layer for logs

--- packages/shared-utils/src/logData.ts
+++ packages/shared-utils/src/logData.ts
@@ -21,6 +21,7 @@ type LayersAvailable =
     | "video_streaming"
     | "video_streaming_subtitles"
     | "bug_report"
+    | "webhooks_received"
     | "*"; // This layer will always be logged, use it for heavy errors
 
 export interface LogDataParams {
