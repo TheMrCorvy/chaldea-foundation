633438038e5c31d1ad06e205a921c500fd614326
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-05-29 07:30:29 -0300

    removed unnecessary files

--- apps/private-cloud-backend/src/utils/separateArrays.ts
+++
@@ -1,9 +0,0 @@
-const separateArrays = <T>(array: T[], size: number): T[][] => {
-    const result: T[][] = [];
-    for (let i = 0; i < array.length; i += size) {
-        result.push(array.slice(i, i + size));
-    }
-    return result;
-};
-
-export default separateArrays;
