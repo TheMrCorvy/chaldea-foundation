48eed8c6598cb5afb9bf40b439167a950f425c6d
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-06-26 15:25:58 -0300

    created single type for auto-updating my resume

---
+++ apps/chaldea-foundation-center/src/api/updated-resume/controllers/updated-resume.ts
@@ -0,0 +1,9 @@
+/**
+ * updated-resume controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController(
+    "api::updated-resume.updated-resume"
+);
---
+++ apps/chaldea-foundation-center/src/api/updated-resume/routes/updated-resume.ts
@@ -0,0 +1,7 @@
+/**
+ * updated-resume router
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreRouter("api::updated-resume.updated-resume");
