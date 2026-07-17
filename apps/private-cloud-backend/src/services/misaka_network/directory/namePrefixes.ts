77d6821b5c0290629ebd07bf0bb46bdec2124ad8
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-07-17 09:16:17 -0300

    created util for name prefixes

---
+++ apps/private-cloud-backend/src/services/misaka_network/directory/namePrefixes.ts
@@ -0,0 +1,39 @@
+export const PREFIXES = {
+    ADULTS: '! ',
+    EXPLICIT: '* ',
+} as const;
+
+export function hasPrefix(name: string): boolean {
+    if (name.startsWith(PREFIXES.ADULTS)) {
+        return true;
+    }
+
+    if (name.startsWith(PREFIXES.EXPLICIT)) {
+        return true;
+    }
+
+    return false;
+}
+
+export interface HasSpecificPrefixParams {
+    name: string;
+    prefix: (typeof PREFIXES)[keyof typeof PREFIXES];
+}
+
+export type HasSpecificPrefix = (params: HasSpecificPrefixParams) => boolean;
+
+export const hasSpecificPrefix: HasSpecificPrefix = ({ name, prefix }) => {
+    return name.startsWith(prefix);
+};
+
+export function cleanName(name: string): string {
+    if (name.startsWith(PREFIXES.ADULTS)) {
+        return name.slice(PREFIXES.ADULTS.length);
+    }
+
+    if (name.startsWith(PREFIXES.EXPLICIT)) {
+        return name.slice(PREFIXES.EXPLICIT.length);
+    }
+
+    return name;
+}
