47bd18186cdd885c818c3be05e730caf69b2aa3a
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-07-21 20:21:17 -0300

    updated jobs report schema

--- packages/type-definitions/src/jobs.ts
+++ packages/type-definitions/src/jobs.ts
@@ -7,8 +7,11 @@ export interface Report {
     id: number;
     title: string;
     description: string;
-    media: ImageComponent;
-    state: JobState;
-    type_of_report: string;
-    job_id: number;
+    media: ImageComponent | null;
+    state: JobState | null;
+    type_of_report: string | null;
+    job_id: number | null;
+    raw_payload: any | null; // The raw payload from the job, which can be of any type
+    created_at: string;
+    updated_at: string;
 }
