f82942fb482abbc22dd1beafb00e298321cbbbd4
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-07-27 18:17:39 -0300

    now directpry processor service supports dot-notation

--- apps/private-cloud-backend/src/services/misaka_network/directory/namePrefixes.ts
+++ apps/private-cloud-backend/src/services/misaka_network/directory/namePrefixes.ts
@@ -6,11 +6,11 @@ export const PREFIXES = {
 } as const;
 
 export function hasPrefix(name: string): boolean {
-    if (name.startsWith(PREFIXES.ADULTS)) {
+    if (name.startsWith(PREFIXES.ADULTS) || name.startsWith('.' + PREFIXES.ADULTS)) {
         return true;
     }
 
-    if (name.startsWith(PREFIXES.EXPLICIT)) {
+    if (name.startsWith(PREFIXES.EXPLICIT) || name.startsWith('.' + PREFIXES.EXPLICIT)) {
         return true;
     }
 
@@ -25,16 +25,16 @@ export interface HasSpecificPrefixParams {
 export type HasSpecificPrefix = (params: HasSpecificPrefixParams) => boolean;
 
 export const hasSpecificPrefix: HasSpecificPrefix = ({ name, prefix }) => {
-    return name.startsWith(prefix);
+    return name.startsWith(prefix) || name.startsWith('.' + prefix);
 };
 
 export function cleanName(name: string): string {
-    if (name.startsWith(PREFIXES.ADULTS)) {
-        return name.slice(PREFIXES.ADULTS.length);
+    if (name.startsWith(PREFIXES.ADULTS) || name.startsWith('.' + PREFIXES.ADULTS)) {
+        return name.startsWith('.') ? name.slice(1 + PREFIXES.ADULTS.length) : name.slice(PREFIXES.ADULTS.length);
     }
 
-    if (name.startsWith(PREFIXES.EXPLICIT)) {
-        return name.slice(PREFIXES.EXPLICIT.length);
+    if (name.startsWith(PREFIXES.EXPLICIT) || name.startsWith('.' + PREFIXES.EXPLICIT)) {
+        return name.startsWith('.') ? name.slice(1 + PREFIXES.EXPLICIT.length) : name.slice(PREFIXES.EXPLICIT.length);
     }
 
     return name;
